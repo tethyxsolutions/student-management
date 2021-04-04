2e057c5255a1d72bb3f7eed8d23ae0ef9a02fb1f
vvp <vishnu@epixelsolutions.com>
2021-04-04 12:57:31 +0530

    db changes

--- student_management_system/settings.py
+++ student_management_system/settings.py
@@ -88,18 +88,18 @@ WSGI_APPLICATION = 'student_management_system.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'student_management',
-        'USER': "root",
-        'PASSWORD': "root",
-        'HOST': '127.0.0.1',
-        'PORT': '3306'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'student_management',
+    #     'USER': "root",
+    #     'PASSWORD': "root",
+    #     'HOST': '127.0.0.1',
+    #     'PORT': '3306'
+    # }
 }
 
 
